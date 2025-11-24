f1384f02e05d0045d1da379d63b8d7aaac25a089
kamal126 <kamalchandra265@gmail.com>
2025-11-24 14:40:48 +0530

    feat: enhance HeroSection with animations and speed up testimonials

--- components/TestimonialCards.tsx
+++ components/TestimonialCards.tsx
@@ -43,7 +43,7 @@ function MusicSchoolTestimonials() {
             <InfiniteMovingCards
                 items={musicSchoolTestimonials}
                 direction="right"
-                speed="slow"
+                speed="fast"
       />
             </div>
         </div>
